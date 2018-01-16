e45dec36864956c871c711609e92d464845bd98c
Esteban Guzmán Zerafín <eguzman@ia.com.mx>
2018-01-16 16:04:24 -0600

    Creación pago pay pal

---
+++ Marinares.Data/Shared/Payment/ItemPayPal.cs
@@ -0,0 +1,11 @@
+﻿namespace Marinares.Data.Shared.Payment
+{
+	public class ItemPayPal
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int Quantity { get; set; }
+		public float Price { get; set; }
+	}
+}
